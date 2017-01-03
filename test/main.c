3d50430bb5a3a3f81de89699cac4e9a8848b718c
guoqichao <guo.qichao@21vianet.com>
2017-01-03 09:48:32 +0800

    and db and port args

--- test/main.c
+++ test/main.c
@@ -4,8 +4,15 @@
 
 int notify(const char* s_msg);
 
-int main() {
-    start_vkey(notify);
+int main(char** args,int arg) {
+    char* db;
+    char* port;
+    if(arg>0)
+        db=args[0];
+    if(arg>1)
+        port=args[1];
+
+    start_vkey(notify,db,port);
     return 0;
 }
 
